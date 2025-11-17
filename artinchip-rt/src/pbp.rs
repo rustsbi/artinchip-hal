93ed1ca8306b3cda1c652d4dae63a7e7d165e0ea
Chongbing Yu <nanahigh@openatom.club>
2025-11-17 15:58:25 +0800

    fix(rt): initialize T-Head and RISC-V floating point in PBP start function
    - Enables D-cache and I-cache to improve execution speed.
    - Initializes float unit.
    - Implements platform specific halt operation.
    
    Signed-off-by: Chongbing Yu <nanahigh@openatom.club>
    Co-authored-by: Zhouqi Jiang <luojia@openatom.club>

--- artinchip-rt/src/pbp.rs
+++ artinchip-rt/src/pbp.rs
@@ -19,34 +19,76 @@ pub static PBP_HEADER: PbpHeader = PbpHeader {
     checksum: 0x0, // <- Real checksum filled by PBP tools.
 };
 
+#[unsafe(link_section = ".bss.uninit")]
+static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
+const STACK_SIZE: usize = 1024; // 1 KiB
+
+const MXSTATUS: u16 = 0x7c0;
+const MHCR: u16 = 0x7c1;
+const MHINT: u16 = 0x7c5;
+const MEXSTATUS: u16 = 0x7e1;
+
 #[unsafe(naked)]
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text.entry")]
 pub extern "C" fn _start() {
-    const STACK_SIZE: usize = 1024; // 1 KiB
-
-    #[unsafe(link_section = ".bss.uninit")]
-    static mut STACK: [u8; STACK_SIZE] = [0u8; STACK_SIZE];
-
     naked_asm!(
-        // Clear `.bss` section
+        // 1. Disable interrupt
+        "   csrw    mie, zero",
+
+        // 2. Hart specific initialization
+        // Best performance register configuration:
+        // - Enable T-Head caches (BTB=1, BPE=1, RS=1, WA=1, WB=1, DE=1, IE=1)
+        // in `mhcr` register.
+        // - Enable T-Head hint operations (PREF_N=3, AMR=1, D_PLD=1)
+        // in `mhint` register.
+        // - Enable T-Head instruction sets (THEADISAEE) and
+        // misaligned access (MM) in `mxstatus` register.
+        // TODO SPUSHEN and SPSWAPEN in `mexstatus` once we have trap handler
+        "   li      t0, 0x103f
+            csrw    {mhcr}, t0
+            li      t1, 0x600c
+            csrs    {mhint}, t1
+            li      t2, 0x408000
+            csrs    {mxstatus}, t2",
+
+        // 3. Initialize float point unit
+        "   li      t0, 0x4000
+            li      t1, 0x2000
+            csrc    mstatus, t0
+            csrs    mstatus, t1
+            csrw    fcsr, zero",
+
+        // 4. Clear `.bss` section
         "   la      t0, sbss
             la      t1, ebss
-        2:  bgeu    t0, t1, 3f
+        1:  bgeu    t0, t1, 2f
             sw      zero, 0(t0)
             addi    t0, t0, 4
-            j       2b",
-        "3:",
-        // Prepare programming language stack
-        "   la      sp, {stack} + {stack_size}",
-        // Start Rust main function
+            j       1b",
+
+        // 5. Prepare programming language stack
+        "2: la      sp, {stack} + {stack_size}",
+
+        // 6. Start Rust main function
         "   j       {main}",
-        // Platform halt if main function returns
-        "4: wfi
-        j       4b",
+
+        // 7. Platform halt (by loop-wfi) if main function returns
+        // Set T-Head wfi behavior to deep-sleep, disable interrupt then
+        // loop-wfi. Clears LPMD=0 and WFEEN=0 in `mexstatus`.
+        "   li      t0, 0x1c
+            csrc    {mexstatus}, t0
+            csrci   mstatus, 0x8
+        3:  wfi
+            j       3b",
+
         stack_size = const STACK_SIZE,
         stack      =   sym STACK,
         main       =   sym pbp_main,
+        mxstatus   =   const MXSTATUS,
+        mhcr       =   const MHCR,
+        mhint      =   const MHINT,
+        mexstatus  =   const MEXSTATUS,
     )
 }
 
